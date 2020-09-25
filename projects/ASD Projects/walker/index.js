eb14db4c479e3df58b47089cf716488b80032f93
DarshanKumar2004 <kumardarshan425@gmail.com>
2020-09-25 01:08:30 +0000

    walker not working

---
+++ projects/ASD Projects/walker/index.js
@@ -0,0 +1,94 @@
+/* global $, sessionStorage */
+
+$(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
+  
+function runProgram(){
+  ////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////// SETUP /////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+
+  // Constant Variables
+  var FRAMES_PER_SECOND_INTERVAL = 1000 / 60;
+
+  var KEY = {
+    "UP": 38,
+    "DOWN": 40,
+    "LEFT": 37,
+    "RIGHT": 39,
+    }
+
+
+
+  // Game Item Objects
+    var positionX = 0;
+    var positionY = 0;
+    var speedX = 0;
+    var speedY = 0;
+
+  // one-time setup
+  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
+  $(document).on('keyDown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////// CORE LOGIC ///////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+
+  /* 
+  On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
+  by calling this function and executing the code inside.
+  */
+  function newFrame() {
+    repositionGameItem();
+    redrawGameItem();
+  }
+  
+  /* 
+  Called in response to events.
+  */
+  function handleKeyDown(event) {
+
+    if (event.which === KEY.DOWN) {
+        speedY = -5;
+        console.log("down pressed");
+    }
+
+    if (event.which === KEY.UP) {
+        speedY = 5;
+        console.log("up pressed");
+    }
+
+    if (event.wich === KEY.LEFT) {
+        speedX = -5;
+        console.log("left pressed");
+    }
+
+    if (event.wich === KEY.RIGHT) {
+        speedX = 5;
+        console.log("right pressed");
+    }
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+    function repositionGameItem() {
+         positionX += speedX;
+         positionY += speedY;
+    }
+
+    function redrawGameItem() {
+        $("#box").css("left", positionX);
+        $("#box").css("top", positionY);
+     }
+  
+  function endGame() {
+    // stop the interval timer
+    clearInterval(interval);
+
+    // turn off event handlers
+    $(document).off();
+  }
+
+  
+  
+}
