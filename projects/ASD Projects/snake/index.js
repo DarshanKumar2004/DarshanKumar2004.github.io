a5a22f05580d8d4081b120b5c7ffa2f1bf7d9c8d
DarshanKumar2004 <kumardarshan425@gmail.com>
2020-11-18 00:19:07 +0000

    save

--- projects/ASD Projects/snake/index.js
+++ projects/ASD Projects/snake/index.js
@@ -47,12 +47,12 @@ function runProgram(){
   redrawApple();
 
   function newFrame() {
-    doCollide();
     repositionHead();
     redrawSnake();
     border();
     scoreDisplay();
     follow();
+    doCollide();
   }
   
   /* 
@@ -182,6 +182,13 @@ function runProgram(){
         apple.y = locationY * 20;
         console.log(apple.x + ' X apple');
         console.log(apple.y + ' Y apple');
+
+        for (var i = 0; i < snakeArray.length;i++) {
+            if (doCollide(apple, snakeArray[i])) {
+            repositionApple();
+            break;
+            }
+        }
     }
 
   function redrawApple() {
@@ -206,7 +213,23 @@ function runProgram(){
     return gameItem;
   }
 
-  function doCollide() {
+  function doCollide(obj1, obj2) {
+    if (obj1.x === obj2.x && obj1.y === obj2.y) {
+        prompt('GAME OVER! Your score was ' + points);
+        head.x = 100;
+        head.y = 100;
+        head.speedX = 0;
+        head.speedY = 0;
+        points = 0;
+        repositionApple();
+        redrawApple();
+        return true;
+    }
+    else {
+        return false;
+    }
+
+    /*
     headC = {};
     headC.leftX = head.x;
     headC.rightX = head.x + head.width;
@@ -225,11 +248,11 @@ function runProgram(){
     bodyC.topY =  $body.y;
     bodyC.bottomY = $body.y + $body.top;
 
-    tail= {};
-    tailleftX = tail.x;
-    tailrightX = tail.x + tail.width;
-    tailtopY =  tail.y;
-    tailbottomY = tail.y + tail.top;
+    tailC= {};
+    tailC.leftX = tail.x;
+    tailC.rightX = tail.x + tail.width;
+    tailC.topY =  tail.y;
+    tailC.bottomY = tail.y + tail.top;
 
     if (((headC.rightX > appleC.leftX) && (headC.leftX < appleC.rightX) && 
         (headC.topY < appleC.bottomY) && (headC.bottomY > appleC.topY))) {
@@ -239,7 +262,24 @@ function runProgram(){
         console.log("collision detected");
         console.log(points);
     }
+    if (((headC.rightX > bodyC.leftX) && (headC.leftX < bodyC.rightX) && 
+        (headC.topY < bodyC.bottomY) && (headC.bottomY > bodyC.topY))) {
+        head.x = 100;
+        head.y = 100;
+        head.speedX = 0;
+        head.speedY = 0;
+        points = 0;
+    }
+    if (((headC.rightX > tailC.leftX) && (headC.leftX < tailC.rightX) && 
+        (headC.topY < tailC.bottomY) && (headC.bottomY > tailC.topY))) {
+        head.x = 100;
+        head.y = 100;
+        head.speedX = 0;
+        head.speedY = 0;
+        points = 0;
+    }
 
+*/
   }
 
   function endGame() {
