77d3a30289f7925195930dbec8a8584e598e331b
DarshanKumar2004 <kumardarshan425@gmail.com>
2021-02-23 17:07:20 -0600

    save

---
+++ projects/Raspberry-Pie/are-you-being-served/requestServer.js
@@ -0,0 +1,12 @@
+// requestServer.js file
+
+const http = require("http");
+const request = require("request");
+
+var port = 8686;
+
+http.createServer(function(req, res){
+
+    
+
+}).listen(port);
