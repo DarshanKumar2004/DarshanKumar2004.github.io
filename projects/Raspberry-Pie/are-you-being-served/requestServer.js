31d926a7a2000e0db6a9def2f42f87d2add81ff0
DarshanKumar2004 <kumardarshan425@gmail.com>
2021-02-23 17:34:00 -0600

    save

--- projects/Raspberry-Pie/are-you-being-served/requestServer.js
+++ projects/Raspberry-Pie/are-you-being-served/requestServer.js
@@ -3,10 +3,18 @@
 const http = require("http");
 const request = require("request");
 
-var port = 8686;
+const port = 8686;
+
 
 http.createServer(function(req, res){
 
-    
+    request("https://DarshanKumar2004/DarshanKumar2004.github.io", function(err, resp, body) {
+        if (!err === true && resp.statusCode === 200) {
+            console.log(body.data);
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.write(error)
+            res.end;
+        }
+    })
 
 }).listen(port);
