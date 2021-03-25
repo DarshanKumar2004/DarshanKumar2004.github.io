a67af951a24fcbb30368b3cb3d53b9d944429852
DarshanKumar2004 <kumardarshan425@gmail.com>
2021-03-25 17:45:02 -0500

    save

---
+++ projects/Raspberry-Pie/first-slice-of-pi/plugins/internal/pirPlugin.js
@@ -0,0 +1,16 @@
+var resources = require('./../../resources/model');
+var Gpio = require('onoff').Gpio;
+
+var sensor;
+var device = resources.pi.sensors.pir;
+
+function connectHardware(){
+        sensor = new Gpio(device.gpio, 'in', 'both');
+        sensor.watch(function(err, value){
+                device.value = !!value;
+        });
+        exports.start = function (params) {};
+        function stop(){
+                sensor.unexport();
+        }
+}
