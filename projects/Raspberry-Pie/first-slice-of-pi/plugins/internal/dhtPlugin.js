ba702b4da5f58374025b155853e611fe344a174e
DarshanKumar2004 <kumardarshan425@gmail.com>
2021-03-25 17:25:58 -0500

    adding first-slice-of-pi

---
+++ projects/Raspberry-Pie/first-slice-of-pi/plugins/internal/dhtPlugin.js
@@ -0,0 +1,27 @@
+var resources = require('./../../resources/model');
+var sensorDriver = require('node-dht-sensor');
+
+var interval, sensor;
+var device = resources.pi.sensors.dht;
+var localParams = {'frequency': 2000};
+
+function connectHardware(){
+        sensor = {
+                "initialize": sensorDriver.initialize(device.model, device.gpio),
+                "read": sensorDriver.read()
+        }
+        device.temperature.value = parseFloat(readout.temperature);
+        device.humidity.value = parseFloat(readout.humidity);
+        sensor.initialize();
+        sensor.read();
+        interval = setInterval(function () {
+                sensor.read();
+        }, localParams.frequency);
+}
+
+function start(parms){
+        localParams = params ? params : localParams;
+        connectHardware();
+        clearInterval(interval);
+
+}
