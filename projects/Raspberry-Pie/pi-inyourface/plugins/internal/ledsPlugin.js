f178bbd1d1101f52a628be6f97c1a77745ad0ee5
DarshanKumar2004 <kumardarshan425@gmail.com>
2021-05-11 17:54:24 -0500

    save

--- projects/Raspberry-Pie/pi-inyourface/plugins/internal/ledsPlugin.js
+++ projects/Raspberry-Pie/pi-inyourface/plugins/internal/ledsPlugin.js
@@ -5,6 +5,17 @@ let actuator1, actuator2;
 let model = resources.pi.actuators.leds;
 let pluginName = resources.pi.actuators.leds[1].name + ", " + resources.pi.actuators.leds[2].name;
 
+function connectHardware() {
+	actuator1 = new Gpio(device.gpio, 'in', 'both');
+	actuator1.watch(function (err, value) {
+		device.value = !!value;
+	});
+	actuator2 = new Gpio(device.gpio, 'in', 'both');
+	actuator2.watch(function (err, value) {
+		device.value = !!value;
+	});
+}
+
 exports.start = function (params) {
 	connectHardware();
 	console.log("starting " + pluginName + " plugin");
