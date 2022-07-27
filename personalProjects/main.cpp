a82d4956c7aba7ebc319342e4a879622c1d3c349
DarshanKumar2004 <kumardarshan425@gmail.com>
2022-07-27 13:43:05 -0500

    changes to site

---
+++ personalProjects/main.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+#include<iostream>
+
+int main()
+{
+    
+    
+    system("pause");
+    return 0;
+}
---
+++ personalProjects/main.hpp
@@ -0,0 +1,8 @@
+// my_class.h
+using namespace std;
+    class my_class {
+    public:
+        void do_something();
+    private:
+        void yea();
+    };
